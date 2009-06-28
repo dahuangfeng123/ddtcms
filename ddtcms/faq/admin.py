e25e6b9a18e7e4c6cc5cdff0ffb1c3c051a3d5da
huaitwooos <huaitwooos@1a5baef8-aed2-11dd-82eb-ff4e721a02f4>
2009-06-28 12:01:17 +0000

    delete unusable files

--- ddtcms/faq/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from ddtcms.faq.models import Faq
-
-
-
-
-
-admin.site.register(Faq)
-
--- ddtcms/tests/blog_category.py
+++
@@ -1,7 +0,0 @@
-from ddtcms import settings
-from ddtcms.blog.models import Entry
-from ddtcms.category.models import Category
-from django.contrib.contenttypes.models import ContentType
-e1=ContentType.objects.get(app_label='blog', model='entry')
-c1=Category.objects.get_content_type(e1)
-print c1
