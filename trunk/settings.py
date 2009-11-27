cd2a580601fb1e7a50cf3e53867cd5e801d7fc55
huaitwooos <huaitwooos@1a5baef8-aed2-11dd-82eb-ff4e721a02f4>
2009-11-27 14:45:20 +0000

    更新了css 删除了news的views.py 添加了一些图片，把link复制到yaml下

--- trunk/settings.py
+++ trunk/settings.py
@@ -15,7 +15,6 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
 DATABASE_NAME = './data.db'             # Or path to database file if using sqlite3.
-#DATABASE_NAME = './examples.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
